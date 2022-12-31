142dd258b689a5129e581a264fc424e99e8a26cf
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-12-30 18:24:43 -0600

    try hash router + remove homepage from package.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import CountriesProvider from './context/CountriesContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<BrowserRouter basename='/REST-Countries-API-with-color-theme-switcher'>
+		<HashRouter>
 			<CountriesProvider>
 				<App />
 			</CountriesProvider>
-		</BrowserRouter>
+		</HashRouter>
   	</React.StrictMode>
 );
